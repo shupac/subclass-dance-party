969b54a4484d67044b6b6cbda6bdb5436f4e40f2
Hack Reactor Students <github@hackreactor.com>
2013-09-30 18:03:30 -0700

    Converted to pseudoclassical instantiation pattern

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,35 +1,36 @@
-describe("blinkyDancer", function() {
+describe("blinkyDancerInst", function() {
 
-  var blinkyDancer;
+  var blinkyDancerInst;
   var timeBetweenSteps = 100;
   var clock;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancerInst = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function(){
-    expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+    expect(blinkyDancerInst.$node).to.be.an.instanceof(jQuery);
   });
 
   it("should have a step function that makes its node blink", function() {
-    sinon.spy(blinkyDancer.$node, 'toggle');
-    blinkyDancer.step();
-    expect(blinkyDancer.$node.toggle.called).to.be.true;
+    sinon.spy(blinkyDancerInst.$node, 'toggle');
+    blinkyDancerInst.step();
+    expect(blinkyDancerInst.$node.toggle.called).to.be.true;
   });
 
   describe("dance", function(){
     it("should call step at least once per second", function(){
-      sinon.spy(blinkyDancer, "step");
-      expect(blinkyDancer.step.callCount).to.be.equal(0)
+      sinon.spy(blinkyDancerInst, "step");
+      blinkyDancerInst.step();
+      expect(blinkyDancerInst.step.callCount).to.be.equal(0)
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
-      expect(blinkyDancer.step.callCount).to.be.equal(1);
+      expect(blinkyDancerInst.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
-      expect(blinkyDancer.step.callCount).to.be.equal(2);
+      expect(blinkyDancerInst.step.callCount).to.be.equal(2);
     });
   });
 });
