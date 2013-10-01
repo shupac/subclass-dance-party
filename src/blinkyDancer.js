8613cef4f4a13c50914150d02d10611b5b2337ad
Hack Reactor Students <github@hackreactor.com>
2013-09-30 18:27:11 -0700

    Implemented blinky dancer with pseudoclassical instantiation

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,6 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
-
+  this.step();
   // we plan to over write the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 };
--- src/dancer.js
+++ src/dancer.js
@@ -2,7 +2,7 @@
 var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  //this.step();
+  
   this.top = top;
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
@@ -14,7 +14,7 @@ var Dancer = function(top, left, timeBetweenSteps){
 Dancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left){
