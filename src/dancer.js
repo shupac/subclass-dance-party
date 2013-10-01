e2214d01db03fbec3df3ef3e53274c0031bfbce9
Hack Reactor Students <github@hackreactor.com>
2013-09-30 21:00:47 -0700

    Updated line up for movers

--- src/dancer.js
+++ src/dancer.js
@@ -2,7 +2,6 @@
 var Dancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  
   this.top = top;
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
@@ -21,6 +20,8 @@ Dancer.prototype.setPosition = function(top, left){
   /* Use css top and left properties to position our <span> tag
    * where it belongs on the page. See http://api.jquery.com/css/
    */
+  this.top = top;
+  this.left = left;
   var styleSettings = {
     top: top,
     left: left
